bf52f64445d81d60d9506d1b113bbacb49aaf099
weber75614433 <weber756144@gmail.com>
2023-03-24 18:59:27 +0800

    [add] fixing code

--- src/main/java/com/weber/backup/table/TableBase.java
+++ src/main/java/com/weber/backup/table/TableBase.java
@@ -1,8 +1,11 @@
 package com.weber.backup.table;
 
+import lombok.Data;
+
 import java.util.Date;
 import java.util.UUID;
 
+@Data
 public class TableBase {
     private UUID uuid;
     private Date createDate;
--- src/main/java/com/weber/backup/table/TableCustom.java
+++ src/main/java/com/weber/backup/table/TableCustom.java
@@ -1,5 +1,8 @@
 package com.weber.backup.table;
 
+import lombok.Data;
+
+@Data
 public class TableCustom extends TableUser{
     private String tel;
     private String address;
